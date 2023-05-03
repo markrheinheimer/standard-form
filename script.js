77fa38139aa96c8296b958cab1c7e323c524f31b
rheins22 <rheinheimers@protonmail.com>
2023-05-03 17:40:51 -0500

    update

--- script.js
+++ script.js
@@ -17,12 +17,36 @@ function showSuccess(input) {
   formControl.className = "form-control success";
 }
 
+//Check email is valid
+function isValidEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
 // Event Listeners
 form.addEventListener("submit", function (e) {
   e.preventDefault;
 
   if (username.value === "") {
     showError(username, "Username is required");
+  } else {
+    showSuccess(username);
+  }
+  if (email.value === "") {
+    showError(email, "Email is required");
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "Email is not valid");
+  } else {
+    showSuccess(username);
+  }
+  if (password.value === "") {
+    showError(password, "Password is required");
+  } else {
+    showSuccess(username);
+  }
+  if (password2.value === "") {
+    showError(password2, "Password2 is required");
   } else {
     showSuccess(username);
   }
