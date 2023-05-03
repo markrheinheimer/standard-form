59ed6562091f0143a5edb249fc2830cd97177924
rheins22 <rheinheimers@protonmail.com>
2023-05-03 17:19:26 -0500

    Inital commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+// Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement; // the inputs parent is the div which we need to select
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+function showSuccess(input) {
+  const formControl = input.parentElement; // the inputs parent is the div which we need to select
+  formControl.className = "form-control success";
+}
+
+// Event Listeners
+form.addEventListener("submit", function (e) {
+  e.preventDefault;
+
+  if (username.value === "") {
+    showError(username, "Username is required");
+  } else {
+    showSuccess(username);
+  }
+});
